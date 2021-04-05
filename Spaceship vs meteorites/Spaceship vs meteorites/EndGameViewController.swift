c87cc630b95e83b928d0d215e86bd145185eb49a
Danil Nurgaliev <Danilkchik@gmail.com>
2021-04-05 17:30:10 +0300

    add project files

---
+++ Spaceship vs meteorites/Spaceship vs meteorites/EndGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EndGameViewController.swift
+//  Spaceship vs meteorites
+//
+//  Created by Danil Nurgaliev on 04.04.2021.
+//
+
+import UIKit
+
+class EndGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Spaceship vs meteorites/Spaceship vs meteorites/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spaceship vs meteorites
+//
+//  Created by Danil Nurgaliev on 04.04.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
