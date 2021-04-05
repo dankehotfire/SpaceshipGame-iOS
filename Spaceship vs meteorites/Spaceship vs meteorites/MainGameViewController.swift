80d4c54a1e734d90bb5d3a40163b345fccbb292f
Danil Nurgaliev <Danilkchik@gmail.com>
2021-04-05 18:03:35 +0300

    add UI game menu

--- Spaceship vs meteorites/Spaceship vs meteorites/MainGameViewController.swift
+++ Spaceship vs meteorites/Spaceship vs meteorites/MainGameViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
